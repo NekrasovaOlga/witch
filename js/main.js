2b0a301a7ece047199116d578486e08731d29174
NekrasovaOlga <33664274+NekrasovaOlga@users.noreply.github.com>
2020-02-05 16:28:05 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,5 +4,33 @@ var mySwiper = new Swiper ('.swiper-container', {
     slidesPerView: 2,
     navigation: {
         nextEl: '.slider_button'
+    },
+    breakpoints: {
+        500: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 20
+          }
     }
 })
+
+
+
+function menu(){
+    var btn = document.getElementById("menu");
+    var burger = document.getElementById("burger");
+    btn.onclick = function(){
+        btn.classList.toggle('nav_burger_span_active');
+        if( btn.classList.contains('nav_burger_span_active')){
+          burger.style.display = "block";  
+        }
+        else
+        {
+            burger.style.display = "none";  
+        }
+        
+    }
+}
